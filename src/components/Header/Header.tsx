7342bdd5b41a66c70f198628fbbee642711ce6a4
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-08-07 17:36:55 +0300

    refactor: change image title in Profile. feat: add padding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,10 @@ export const Header = () => {
     return (
         <header className={style.header}>
             <img
-                src="http://cdn.onlinewebfonts.com/svg/img_486696.png"
+                src="http://cdn.onlinewebfonts.com/svg/img_362868.png"
+                //http://cdn.onlinewebfonts.com/svg/img_362868.png
                 alt="logo"/>
+            <span className={style.title}> Connect</span>
         </header>
     );
 };
