2c6e990c549a65833b217389ae9851794b808520
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-08-11 16:29:34 +0300

    feat: add function for create new message on the Messages page (Dialogs component). And add props to Dialogs component and each component is at the top

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,28 @@
-import React, {FC} from 'react';
+import React, {ChangeEvent, FC} from 'react';
 import style from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {DialogsPageType} from "../../redux/State";
 
+export type PropsType = {
+    addMessage: (messageText: string) => void
+    changeMessageText: (newMessage: string) => void
+}
 
-
-export const Dialogs: FC<DialogsPageType> = ({dialogs, messages}) => {
+export const Dialogs: FC<DialogsPageType & PropsType> = ({dialogs, messages, addMessage, changeMessageText,newMessageText}) => {
 
     let dialogsElement = dialogs.map(dialog => <DialogItem key={dialog.id} id={dialog.id} name={dialog.name}/>)
     let messagesElement = messages.map(m => <Message key={m.id} id={m.id} message={m.message}/>)
+
+
+    const addNewMessage = () => {
+        addMessage(newMessageText)
+        changeMessageText('')
+    }
+
+    const onChangeMessageHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        changeMessageText(e.currentTarget.value)
+    }
 
 
     return (
@@ -19,6 +32,10 @@ export const Dialogs: FC<DialogsPageType> = ({dialogs, messages}) => {
             </div>
             <div className={style.messages}>
                 {messagesElement}
+            </div>
+            <div>
+                <textarea value={newMessageText} onChange={onChangeMessageHandler} ></textarea>
+                <button onClick={addNewMessage}>New message</button>
             </div>
         </div>
     );
