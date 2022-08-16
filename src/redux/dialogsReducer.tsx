14c96a02ba8be3576d01cb24c5afa18ee010f4eb
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-08-16 17:27:50 +0300

    feat: add type for state and profileReducer and dialogsReducer

--- src/redux/dialogsReducer.tsx
+++ src/redux/dialogsReducer.tsx
@@ -1,18 +1,44 @@
-import {DialogsPageType, DispatchType, MessageType} from "./Store";
+import {DispatchType} from "./Store";
 
-export const dialogsReducer = (state: DialogsPageType, action:DispatchType) => {
+export type DialogType = {
+    id: number
+    name: string
+}
+export type MessageType = {
+    id: number
+    message: string
+}
+
+export type initialStateType = typeof initialState;
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Kate'},
+        {id: 3, name: 'Alex'},
+        {id: 4, name: 'Mike'},
+        {id: 5, name: 'Helen'},
+    ] as Array<DialogType>,
+    messages: [
+        {id: 1, message: 'Hey, how are you?'},
+        {id: 2, message: 'Are you really?'},
+        {id: 3, message: 'Have a good day!'},
+        {id: 4, message: 'Wake up!'},
+        {id: 5, message: 'Dear, what\'s wrong?'},
+    ] as Array<MessageType>,
+    newMessageText: ''
+}
+
+
+export const dialogsReducer = (state:initialStateType = initialState, action:DispatchType):initialStateType => {
     switch (action.type) {
         case "SEND-MESSAGE" : {
             let newMessage:MessageType  = {
                 id: new Date().getTime(),
-                message: action.payload.newMessageText,}
-            state.messages.push(newMessage)
-            /*state._onChange()*/
-            return state
+                message: state.newMessageText,}
+            return {...state, messages: [...state.messages, newMessage], newMessageText: ''}
         }
         case "CHANGE-MESSAGE-TEXT" : {
-            state.newMessageText  = action.payload.newMessage
-            return state
+            return {...state, newMessageText: action.payload.newMessage}
         }   // Update body new message
         default:
             console.log("Dialogs page wasn't changed")
@@ -22,10 +48,9 @@ export const dialogsReducer = (state: DialogsPageType, action:DispatchType) => {
 }
 
 export type sendNewMessageACType = ReturnType<typeof sendNewMessageAC>
-export const sendNewMessageAC = (newMessageText: string) => {
+export const sendNewMessageAC = () => {
     return {
         type: "SEND-MESSAGE",
-        payload: {newMessageText}
     }as const
 }
 export type changeMessageTextACType = ReturnType<typeof changeMessageTextAC>
--- src/redux/profileReducer.tsx
+++ src/redux/profileReducer.tsx
@@ -1,19 +1,32 @@
-import {DispatchType, PostsType, ProfilePageType} from "./Store";
+import {DispatchType} from "./Store";
 
-export const profileReducer = (state: ProfilePageType, action: DispatchType) => {
+export type PostsType = {
+    id: number
+    message: string
+    likesCount: number
+}
+
+export type initialStateType = typeof initialState;
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 15},
+        {id: 2, message: 'What\'s, wrong?', likesCount: 25},
+    ] as Array<PostsType>,
+    newPostText: '',
+}
+
+export const profileReducer = (state:initialStateType = initialState, action: DispatchType):initialStateType => {
     switch (action.type) {
         case "ADD-POST" : {
             let newPost: PostsType = {
                 id: new Date().getTime(),
-                message: action.payload.newPostText,
+                message: state.newPostText,
                 likesCount: 0}
-            state.posts.push(newPost)
-            /*state._onChange()*/
-            return state
+            //state.posts.push(newPost)
+            return {...state, posts: [...state.posts, newPost], newPostText: ''}
         }
         case "CHANGE-POST-TEXT" : {
-            state.newPostText = action.payload.newPost
-            return state
+           return {...state, newPostText: action.payload.newPost}
 
         }    // Update new post text
         default:
@@ -23,14 +36,14 @@ export const profileReducer = (state: ProfilePageType, action: DispatchType) => 
 }
 
 export type addPostACType = ReturnType<typeof addPostAC>
-export const addPostAC = (newPostText: string) => {
+export const addPostAC = () => {
     return {
         type: "ADD-POST",
-        payload: {newPostText}
     }as const
 }
 export type changePostTextACType = ReturnType<typeof changePostTextAC>
 export const changePostTextAC = (newPost: string) => {
+
     return {
         type: "CHANGE-POST-TEXT",
         payload: {newPost}
