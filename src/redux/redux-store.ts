5cfdee32b4004955632b62c16a0299943194cbb5
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-08-16 17:18:08 +0300

    refactor: add type for rootReducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,13 +2,15 @@ import {combineReducers, createStore} from "redux";
 import {profileReducer} from "./profileReducer";
 import {dialogsReducer} from "./dialogsReducer";
 import {sidebarReducer} from "./sidebar.Reducer";
-import {StoreType} from "./Store";
 
 
-let reducers = combineReducers({
+
+export const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
 })
 
-export let store: StoreType = createStore(reducers)
+export type AppStateType = ReturnType<typeof rootReducer>
+
+export let store = createStore(rootReducer)
