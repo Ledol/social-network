d97ceb1fa514c4b6028fbc44ccfb697d2bc0f193
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-08-16 17:29:56 +0300

    refactor: remove properties from Profile and DialogsContainer. refactor: temporarily commented out parameter Friends

--- src/App.tsx
+++ src/App.tsx
@@ -3,52 +3,37 @@ import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
 import {Header} from "./components/Header/Header";
 import {Profile} from "./components/Profile/Profile";
-import {Dialogs} from "./components/Dialogs/Dialogs";
 import {Route} from "react-router-dom";
 import {Music} from "./components/Music/Music";
 import {News} from "./components/News/News";
 import {Setting} from "./components/Setting/Setting";
-import { StoreType} from "./redux/Store";
 import {Friends} from "./components/Friends/Friends";
+import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
+import {useSelector} from "react-redux";
+import {AppStateType} from "./redux/redux-store";
 
 
+function App() {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            {/* <Friends friends={state.sidebar.friends}/>*/}
+            <div className='app-wrapper-content'>
+                <Route path="/profile" render={() => {
+                    return <Profile/>
+                }}/>
+                <Route path="/dialogs" render={() => {
+                    return <DialogsContainer/>
+                }}/>
+                <Route path="/news" component={News}/>
+                <Route path="/music" component={Music}/>
+                <Route path="/setting" component={Setting}/>
 
-type AppPropsType =  {
-    store: StoreType
-}
-
-
-function App (props: AppPropsType) {
-    const state = props.store.getState()
-  return (
-
-    <div className="app-wrapper">
-        <Header/>
-        <Navbar/>
-        <Friends friends={state.sidebar.friends}/>
-        <div className='app-wrapper-content'>
-            <Route path="/profile" render={()=> {
-                return <Profile posts={state.profilePage.posts}
-                                dispatch={props.store.dispatch.bind(props.store)}
-                                newPostText={state.profilePage.newPostText} />
-            }} />
-            <Route path="/dialogs" render={()=> {
-                return  <Dialogs dialogs={state.dialogsPage.dialogs}
-                                 messages={state.dialogsPage.messages}
-                                 dispatch={props.store.dispatch.bind(props.store)}
-                                 newMessageText={state.dialogsPage.newMessageText}
-                />
-            }} />
-            <Route path="/news" component={News} />
-            <Route path="/music" component={Music} />
-            <Route path="/setting" component={Setting} />
-
+            </div>
         </div>
 
-
-    </div>
-
-  );
+    );
 }
 
 export default App;
