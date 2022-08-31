dd64e7488caf7f6e90ebe0fe14287808676a2b23
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-08-31 11:29:47 +0300

    refactor: add usersPage to rootReducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,16 +1,20 @@
 import {combineReducers, createStore} from "redux";
 import {profileReducer} from "./profileReducer";
 import {dialogsReducer} from "./dialogsReducer";
-import {sidebarReducer} from "./sidebar.Reducer";
+import {sidebarReducer} from "./sidebarReducer";
+import {usersReducer} from "./usersReducer";
 
 
-
+export type AppStateType = ReturnType<typeof rootReducer>
 export const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
+    usersPage: usersReducer,
 })
 
-export type AppStateType = ReturnType<typeof rootReducer>
 
 export let store = createStore(rootReducer)
+
+// @ts-ignore
+window.store = store;
