7ae7141f32f034c40bb3b08d442e1f73a06170e7
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-09-09 09:56:42 +0300

    feat: add second container component(with API) to pure Users component.

---
+++ src/components/Users/UsersAPIContainer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {UsersPropsType} from "./UsersContainer";
+import axios from "axios";
+import {Users} from "./Users";
+
+
+class UsersAPIContainer extends React.Component<UsersPropsType> {
+
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+        onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
+    }
+
+    render() {
+        return <Users totalUserCount={this.props.totalUserCount}
+                      pageSize={this.props.pageSize}
+                      onPageChanged={this.onPageChanged}
+                      currentPage={this.props.currentPage}
+                      users={this.props.users}
+                      unfollow={this.props.unfollow}
+                      follow={this.props.follow}
+
+        />
+    }
+}
+
+export default (UsersAPIContainer);
+
