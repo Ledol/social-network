07a74dd67704154ff1593943bd2626c66ef0f583
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-09-13 21:05:58 +0300

    refactor: move code about preloader to component Preloader

---
+++ src/components/common/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from "../../Users/Users.module.css";
+import preloader from "../../../assets/images/preloader4.gif"
+
+export const Preloader = () => {
+    return <div>
+        <img className={style.preload} src={preloader} alt='preloader'/>
+    </div>
+
+};
+
