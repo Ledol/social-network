1de58aab9b356f294a07ac70885bcd966c0da935
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-09-13 21:08:32 +0300

    feat: add propsType for component

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
+import {ProfileType} from "../../redux/profileReducer";
 
 
-export const Profile = () => {
+type ProfilePropsType = {
+    profile: ProfileType
+}
+
+export const Profile = (props: ProfilePropsType) => {
     return (
         <div>
-            <ProfileInfo/>
+            <ProfileInfo profile={props.profile}/>
             <MyPostsContainer/>
         </div>
     );
