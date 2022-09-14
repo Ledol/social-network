c4f30d9262ec4b5338d8bdbae0e3220f1812763f
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-09-14 10:15:23 +0300

    refactor: change path for ProfileContainer, add userId

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
 import {Header} from "./components/Header/Header";
-import {Profile} from "./components/Profile/Profile";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 import {Route} from "react-router-dom";
 import {Music} from "./components/Music/Music";
 import {News} from "./components/News/News";
@@ -19,7 +19,7 @@ function App() {
             <Navbar/>
             <FriendsContainer/>
             <div className='app-wrapper-content'>
-                <Route path="/profile" render={() => <Profile/>}/>
+                <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
                 <Route path="/dialogs" render={() => <DialogsContainer/>}/>
                 <Route path="/users" render={() => <UsersContainer/>}/>
                 <Route path="/news" component={News}/>
