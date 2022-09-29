a5bd10dbf59e7972865a508aaa65a83f17cc74bd
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-09-29 14:16:04 +0300

    feat: -create auth reducer for connect login status

---
+++ src/redux/authReducer.ts
@@ -0,0 +1,42 @@
+
+export type DataType = {
+    id: null|number
+    login: null|string
+    email: null|string
+}
+export type initialStateType = {
+    data: DataType
+    isAuth: boolean
+}
+
+let initialState = {
+    data: {
+        id: null,
+        login: null,
+        email: null,
+    },
+        isAuth: false
+}
+
+export type actionType = setAuthUserDataType
+
+export const authReducer = (state: initialStateType = initialState, action: actionType): initialStateType => {
+    switch (action.type) {
+        case 'SET_USER_DATA': {
+            return {...state, data: action.payload.data, isAuth: true}
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+
+export type setAuthUserDataType = ReturnType<typeof setAuthUserData>
+export const setAuthUserData = (data: DataType) => {
+    return {
+        type: 'SET_USER_DATA',
+        payload: {data}
+    } as const
+}
+
