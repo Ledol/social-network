57c655b67a0cea1f986966a310a5fef2c562be6a
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-10-10 15:01:31 +0300

    feat: - created HOC withAuthRedirect. - added check to login.

---
+++ src/hoc/withAuthRedirect.tsx
@@ -0,0 +1,27 @@
+import React, {ComponentType} from 'react';
+import {connect} from "react-redux";
+import {AppStateType} from "../redux/redux-store";
+import {Redirect} from "react-router-dom";
+
+type mapStateToPropsType = {
+    isAuth: boolean
+}
+let mapStateToProps = (state: AppStateType): mapStateToPropsType => ({
+    isAuth: state.auth.isAuth
+})
+
+export function withAuthRedirect <T>(Component: ComponentType<T>) {
+
+    const RedirectComponent = (props: mapStateToPropsType) => {
+    let {isAuth, ...restProps} = props
+
+        if (!props.isAuth) return <Redirect to="/login"/>
+
+        return <Component {...restProps as T}/>
+    }
+
+
+    let ConnectedRedirectComponent = connect(mapStateToProps)(RedirectComponent)
+
+    return ConnectedRedirectComponent
+}
