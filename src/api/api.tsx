6d957d5456cfff3c1fc9eeedd5ff34e96e79eeee
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-10-14 14:15:02 +0300

    feat: - added instance with baseURL and settings

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,37 +1,31 @@
 import axios from "axios";
 
-const baseURL = `https://social-network.samuraijs.com/api/1.0/`;
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '36c6d8e6-c7f6-48a8-984f-4e1b0d11ee5b',
+    },
+})
 
 export const usersAPI = {
     getUsers (currentPage: number, pageSize: number) {
-        return axios.get(`${baseURL}users?page=${currentPage}&count=${pageSize}`, {
-            withCredentials: true
-        })
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then((response) => {
                 return response.data;
             })
     },
     unfollowUser (userId: number) {
-        return axios.delete(`${baseURL}follow/${userId}`, {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "36c6d8e6-c7f6-48a8-984f-4e1b0d11ee5b"
-            }
-        })
+        return instance.delete(`follow/${userId}`)
     },
     followUser (userId: number) {
-        return  axios.post(`${baseURL}follow/${userId}`, {}, {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "36c6d8e6-c7f6-48a8-984f-4e1b0d11ee5b"
-            }
-        })
+        return  instance.post(`follow/${userId}`, {})
     }
 }
 
 export const profileAPI = {
     getProfile (userId: string) {
-        return axios.get(`${baseURL}profile/` + userId)
+        return instance.get(`profile/` + userId)
             .then(response => {
                 return response.data
             })
@@ -40,9 +34,7 @@ export const profileAPI = {
 
 export const authAPI = {
     getAuth() {
-        return axios.get(`${baseURL}auth/me`, {
-            withCredentials: true
-        })
+        return instance.get(`auth/me`)
     }
 }
 
