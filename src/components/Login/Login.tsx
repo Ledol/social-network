ff9ae742de699740f863a0c27310fb1dfdbad97c
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-10-31 12:33:42 +0300

    feat: - added HOC connect to Login component. - added mapStateToProps and mapDispatchToProps. - added type to loginTC - added logic for submit data about user

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {FormDataType, LoginReduxForm} from "./LoginForm";
+import {connect} from "react-redux";
+import {loginTC} from "../../redux/authReducer";
+import {AppStateType} from "../../redux/redux-store";
+import {Redirect} from "react-router-dom";
 
+type LoginType = MapStateToPropsType & {
+    loginTC: (email: string, password: string, rememberMe: boolean) => void
+}
 
-
-export const Login = () => {
+export const Login = (props: LoginType) => {
 
     const onSubmitHandler = (formData: FormDataType) => {
-        console.log(formData)
+        props.loginTC(formData.email, formData.password, formData.rememberMe)
+    }
+    if(props.isAuth) {
+        return <Redirect to={"/profile"}/>
     }
     return (
         <div>
@@ -15,4 +24,16 @@ export const Login = () => {
         </div>
     );
 };
+type MapStateToPropsType = {
+    isAuth: boolean
+}
+
+const mapStateToProps = (state: AppStateType): MapStateToPropsType => {
+    return{
+        isAuth: state.auth.isAuth
+    }
+}
+
+
+export const LoginContainer  = connect(mapStateToProps, {loginTC})(Login)
 
--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -4,7 +4,7 @@ import {Input} from "../common/FormsControls/FormsControls";
 import { required} from "../../utils/validators/validators";
 
 export type FormDataType = {
-    login: string
+    email: string
     password: string
     rememberMe: boolean
 }
@@ -14,10 +14,10 @@ export const LoginForm: FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={"Login"} name={"login"} component={Input} validate={[required]}/>
+                <Field placeholder={"Email"} name={"email"} component={Input} validate={[required]}/>
             </div>
             <div>
-                <Field placeholder={"Password"} name={"password"} component={Input} validate={[required]}/>
+                <Field placeholder={"Password"} name={"password"} type={"password"} component={Input} validate={[required]}/>
             </div>
             <div>
                 <Field type="checkbox" name={"rememberMe"} component={Input} /> remember me
